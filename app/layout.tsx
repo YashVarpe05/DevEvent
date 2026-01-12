41ab1444687d8e973223c64721f6b3746e678be6
YashVarpe05 <yashvarpe2005@gmail.com>
2026-01-12 12:39:19 +0530

    Implement PostHog

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/components/LightRays";
 
+import { PostHogProvider } from "./providers";
+
 const schibstedGrotesk = Schibsted_Grotesk({
 	variable: "--font-schibsted-grotesk",
 	subsets: ["latin"],
@@ -25,24 +27,26 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
-			>
-				<div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-					<LightRays
-						raysOrigin="top-center-offset"
-						raysColor="#5dfeca"
-						raysSpeed={0.8}
-						lightSpread={0.95}
-						rayLength={2}
-						followMouse={true}
-						mouseInfluence={0.04}
-						noiseAmount={0.0}
-						distortion={0.02}
-					/>
-				</div>
-				<main>{children}</main>
-			</body>
+			<PostHogProvider>
+				<body
+					className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+				>
+					<div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+						<LightRays
+							raysOrigin="top-center-offset"
+							raysColor="#5dfeca"
+							raysSpeed={0.8}
+							lightSpread={0.95}
+							rayLength={2}
+							followMouse={true}
+							mouseInfluence={0.04}
+							noiseAmount={0.0}
+							distortion={0.02}
+						/>
+					</div>
+					<main>{children}</main>
+				</body>
+			</PostHogProvider>
 		</html>
 	);
 }
